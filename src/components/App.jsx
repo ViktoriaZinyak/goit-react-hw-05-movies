1edd5b61300f94b0919bbd8f55c1a79ad2b94a37
ViktoriyaZinyak <viktoriazinyak@gmail.com>
2022-10-29 13:37:34 +0300

    routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={'Home'} />
+        <Route path="/movies" element={'Movies'} />
+        <Route path="/movies/:movieId" element={'MovieDetails'} />
+      </Routes>
     </div>
   );
 };
